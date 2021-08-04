cc80b07468fa1c0acc951dcd3e1d6466fd2a0681
Guilherme do Valle <guivalle@bitbucket.org>
2021-08-04 16:33:01 -0300

    add mongodb class connection

---
+++ mongo_connection.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+
+
+class MongoConnection:
+    mongo_client = MongoClient()
+    db_lusiadas = mongo_client.lusiadas_chants
+    chants_collections = db_lusiadas.chant
+
+    def insert_chant(self, chant):
+        result_obj = self.chants_collections.insert_one(chant)
+        return result_obj.acknowledged
+
+    def get_chant(self, chant_number, stranza):
+        chant = self.chants_collections.find_one({"chant_number": chant_number, "stranza": stranza})
+        return chant
