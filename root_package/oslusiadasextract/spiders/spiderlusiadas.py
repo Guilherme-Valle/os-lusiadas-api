5eef9855b6eb4301e7e5290fd3348836b6762882
Guilherme do Valle <guivalle@bitbucket.org>
2021-08-06 13:41:49 -0300

    finish extracting to mongodb

--- root_package/oslusiadasextract/spiders/spiderlusiadas.py
+++ root_package/oslusiadasextract/spiders/spiderlusiadas.py
@@ -9,7 +9,6 @@ mongo_connection = MongoConnection()
 class SpiderlusiadasSpider(scrapy.Spider):
     name = 'spiderlusiadas'
     start_urls = utils.generate_chants_links()
-    # start_urls = ['https://oslusiadas.org/i/']
 
     def parse(self, response):
         array_with_text = response.xpath('//div[@class="uk-panel uk-panel-box estrofe"]/descendant::text()').extract()
@@ -17,10 +16,6 @@ class SpiderlusiadasSpider(scrapy.Spider):
         current_url = response.request.url
         chant_info = utils.parse_chant_url(current_url)
         chant_text = ' '.join(utils.parse_scrapy_response(array_with_text)).strip()
-
-        print(chant_info['chant_number'])
-        print(chant_info['stranza'])
-        print(chant_text)
 
         mongo_connection.insert_chant({"chant_number": chant_info['chant_number'],
                                        "stranza": chant_info['stranza'],
